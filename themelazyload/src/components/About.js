79cbf4b24a806e0c1a07e39762dece2658fafdc5
Deepak singh <studydipu@gmail.com>
2025-03-08 10:03:16 +0530

    Added About.js

---
+++ themelazyload/src/components/About.js
@@ -0,0 +1,11 @@
+// About.js (Lazy Loaded Component)
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>This is the about page.</p>
+    </div>
+  );
+};
